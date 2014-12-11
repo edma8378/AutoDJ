fd724da63964f0f100446cccd43e62344adde123
alex-brisnehan <alexander.brisnehan@colorado.edu>
2014-12-10 21:06:10 -0700

    Added ability to loop through list

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,28 +1,70 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('PlaylistCtrl', function($scope, $http) {
-  $http.get('playlists/playlist.json')
-       .then(function(res){
-          $scope.playlists = res.data;
-        });
-});
-
-myApp.controller('musicPlayer', function() {
 	soundManager.setup({
 		url:'swf/',
 		preferFlash: 'false',
 
-		onready: function() {
-			soundManager.createSound({
-				id: 'swing',
-				url:'music/swing.mp3',
+		// onready: function() {
+		// 	soundManager.createSound({
+		// 		id: 'swing',
+		// 		url:'music/swing.mp3',
+		// 		stream: true,
+		// 		whileplaying: function() {
+  //     				var timeLeft = this.duration - this.position;
+  //     				document.getElementById("timeLeft").innerHTML = timeLeft;
+  //    			},
+		// 	});
+		// 	soundManager.play('swing', {volume:50});
+		// },
+	});
+  soundManager.onready(function(){
+    makePlaylist();
+  });
+  var makePlaylist = function() {
+    $http.get('../playlists/2014-12-19/11am.playlist')
+       .then(function(res){
+          $scope.playlists = res.data;
+          $scope.makeMusic(0);
+        });
+     }
+  $scope.makeMusic = function(songId) {
+    $scope.numSong = songId.toString();
+    var json = $scope.playlists;
+    console.log(json[$scope.numSong].path);
+    console.log(json.length);
+    path = "../" + json[songId].path;
+  	soundManager.createSound({
+				id: $scope.numSong,
+				url: path,
 				stream: true,
+        autoLoad: true,
+        autoPlay: true,
 				whileplaying: function() {
       				var timeLeft = this.duration - this.position;
       				document.getElementById("timeLeft").innerHTML = timeLeft;
      			},
+        onfinish: function() {
+              songId++;
+              $scope.makeMusic(songId)
+        }
 			});
-			soundManager.play('swing', {volume:50});
-		},
-	});
+  }
+  $scope.pauseMusic = function() {
+    console.log($scope.numSong);
+    soundManager.togglePause($scope.numSong);
+  }
+
 });
+// for(var i = 0; i < json.length; i++)
+//           {
+//             $scope.flag = true
+//             $scope.makeMusic(i, "../" + json[i].path);
+//             console.log(json[i].artist);
+//             soundManager.play(i, {
+//               onfinish: function() {
+//                 $scope.flag = false;
+//             }})
+//             while(!($scope.flag));
+//             console.log("here");
+//           }
