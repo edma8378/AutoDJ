f93e2a678b1e7e0469645f40d3d58e7978581c2c
alex-brisnehan <alexander.brisnehan@colorado.edu>
2015-02-04 14:07:19 -0700

    Change playlist name in GUI

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -21,7 +21,7 @@ myApp.controller('musicPlayer', function($filter, $scope, $http) {
   soundManager.onready(function(){
     $scope.date = new Date();
     console.log($scope.date);
-    $scope.filt = $filter('date')($scope.date, "yyyy-MM-dd/Ha");
+    $scope.filt = $filter('date')($scope.date, "yyyy-MM-dd/H");
     makePlaylist($scope.filt, 0);
   });
   var makePlaylist = function(date, st) {
@@ -51,7 +51,7 @@ myApp.controller('musicPlayer', function($filter, $scope, $http) {
               };
               if($scope.songNum > $scope.playlists.length - 1){
                 $scope.date.addHours(1);
-                $scope.filt = $filter('date')($scope.date, "yyyy-MM-dd/Ha");
+                $scope.filt = $filter('date')($scope.date, "yyyy-MM-dd/H");
                 console.log($scope.filt);
                 makePlaylist($scope.filt, 1);
               }
@@ -90,7 +90,7 @@ myApp.controller('musicPlayer', function($filter, $scope, $http) {
           if(songId >= $scope.playlists.length - 1)
           {
             $scope.date.addHours(1);
-            $scope.filt = $filter('date')($scope.date, "yyyy-MM-dd/Ha");
+            $scope.filt = $filter('date')($scope.date, "yyyy-MM-dd/H");
             console.log($scope.filt);
             makePlaylist($scope.filt, 1);
           }
