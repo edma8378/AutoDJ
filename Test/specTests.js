e29f907bfacfee677bc62f40b88359150243551e
Kristina Entzel <kren7615@colorado.edu>
2015-03-06 11:31:20 -0700

    Test Dir update

--- Test/specTests.js
+++ Test/specTests.js
@@ -5,14 +5,13 @@ describe('AutoDJ', function() {
 	// Put variables you need to access elements here 
 	// If more test files are added and global variables are needed, add to
  	// protractor_conf.js in the Global variables section
-	//Testing some song functionality with the swing.mp3
-        //var song1Path = '/home/kristi/Documents/AutoDj/AutoDJ/app/music/swing.mp3';
-	//var song1 = document.getElementById('song1Path');
 
 
 	beforeEach(function() {
-		   // Change this to the address you're using
-		   // This much match protractor_conf.js baseURL
+		// Change this to the address you're using
+		// This much match or be a subdirectory of the
+		// protractor_conf.js baseURL
+		// Raspberry pi: browser.get('http://localhost/AutoDJ/app');
 		  browser.get('http://localhost');
 	});
 	
@@ -20,29 +19,10 @@ describe('AutoDJ', function() {
   		expect(browser.getTitle()).toEqual('AutoDJ');
 	});
 
-	//Soundmanager is working
-	it('TEST NOT IMPLEMENTED. should be using soundManager, onready() has occured', function() {
 	
-	});
-
-	//Soundmanager will play from a paht
- 	it('TEST NOT IMPLEMENTED. Should be able to play a url', function(){
-		//expect(soundManager.canPlayURL(song1Path)).toEqual(true);
-	});
-	//Should play an mp3
-	it('TEST NOT IMPLEMENTED. Should be able to play a MIME', function(){
-		//expect(soundManager.canPlayURL(song1Path)).toEqual(true);
-	});
-  
 	it('TEST NOT IMPLEMENTED. Is AutoDJ, and should have loaded playlist', function(){
 
 	});
 
-	it('TEST NOT IMPLEMENTED. Should be the playlist for the correct hour', function(){
-
-	});
-
-	it('TEST NOT IMPLEMENTED. Should let a user log in', function(){
-	});
-
 });
+
